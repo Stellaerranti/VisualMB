a11c3ce69707ca911cb4698e0421047c68738265
Stellaerranti <ushakovda@my.msu.ru>
2024-03-31 23:03:18 +0300

    BasicsAdded

--- VisualMB/VisualMB/Form1.Designer.cs
+++ VisualMB/VisualMB/Form1.Designer.cs
@@ -29,12 +29,232 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.fileButton = new System.Windows.Forms.ToolStripButton();
+            this.tickTime = new System.Windows.Forms.Timer(this.components);
+            this.MainChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.StartStopButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MLabel = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainChart)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.MainChart, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 97.59036F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 377);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // fileButton
+            // 
+            this.fileButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fileButton.Image = ((System.Drawing.Image)(resources.GetObject("fileButton.Image")));
+            this.fileButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(29, 22);
+            this.fileButton.Text = "File";
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // tickTime
+            // 
+            this.tickTime.Tick += new System.EventHandler(this.tickTime_Tick);
+            // 
+            // MainChart
+            // 
+            chartArea1.AxisX.Title = "B";
+            chartArea1.AxisY.Title = "M";
+            chartArea1.Name = "ChartArea1";
+            this.MainChart.ChartAreas.Add(chartArea1);
+            this.MainChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainChart.Location = new System.Drawing.Point(163, 3);
+            this.MainChart.Name = "MainChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.IsVisibleInLegend = false;
+            series1.Name = "MB";
+            this.MainChart.Series.Add(series1);
+            this.MainChart.Size = new System.Drawing.Size(634, 371);
+            this.MainChart.TabIndex = 2;
+            this.MainChart.Text = "chart1";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.fileLabel, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.StartStopButton, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.ClearButton, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.BLabel, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.MLabel, 1, 4);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(154, 163);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(5, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File:";
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fileLabel.Location = new System.Drawing.Point(81, 2);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(68, 30);
+            this.fileLabel.TabIndex = 1;
+            this.fileLabel.Text = "-";
+            // 
+            // StartStopButton
+            // 
+            this.StartStopButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StartStopButton.Location = new System.Drawing.Point(5, 69);
+            this.StartStopButton.Name = "StartStopButton";
+            this.StartStopButton.Size = new System.Drawing.Size(68, 24);
+            this.StartStopButton.TabIndex = 2;
+            this.StartStopButton.Text = "Start";
+            this.StartStopButton.UseVisualStyleBackColor = true;
+            this.StartStopButton.Click += new System.EventHandler(this.StartStopButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ClearButton.Location = new System.Drawing.Point(81, 69);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(68, 24);
+            this.ClearButton.TabIndex = 3;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(5, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "M:";
+            // 
+            // BLabel
+            // 
+            this.BLabel.AutoSize = true;
+            this.BLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BLabel.Location = new System.Drawing.Point(81, 98);
+            this.BLabel.Name = "BLabel";
+            this.BLabel.Size = new System.Drawing.Size(68, 30);
+            this.BLabel.TabIndex = 5;
+            this.BLabel.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(5, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "B:";
+            // 
+            // MLabel
+            // 
+            this.MLabel.AutoSize = true;
+            this.MLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MLabel.Location = new System.Drawing.Point(81, 130);
+            this.MLabel.Name = "MLabel";
+            this.MLabel.Size = new System.Drawing.Size(68, 31);
+            this.MLabel.TabIndex = 7;
+            this.MLabel.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 402);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Please give me the name";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MainChart)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStripButton fileButton;
+        private System.Windows.Forms.Timer tickTime;
+        private System.Windows.Forms.DataVisualization.Charting.Chart MainChart;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Button StartStopButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label BLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label MLabel;
     }
 }
 
--- VisualMB/VisualMB/Form1.cs
+++ VisualMB/VisualMB/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,55 @@ namespace VisualMB
 {
     public partial class Form1 : Form
     {
+        private string outputFile;
+        private bool isFileLinked = false;
+        private bool isOn= false;
+
         public Form1()
         {
             InitializeComponent();
+            saveFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog.RestoreDirectory = true;
+
+            MainChart.Series["MB"].Points.Clear();
+        }
+
+        //Подключаем файл
+        private void fileButton_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel) { return; }
+
+            outputFile = saveFileDialog.FileName;
+            fileLabel.Text = Path.GetFileName(outputFile);
+
+            isFileLinked = true;
+        }
+
+        private void StartStopButton_Click(object sender, EventArgs e)
+        {
+            if (isOn)
+            {
+                StartStopButton.Text = "Start";
+                isOn = false;   
+            }
+            else if (!isOn)
+            {
+                StartStopButton.Text = "Stop";
+                isOn = true;
+            }
+        }
+
+        //Main cycle
+        private void tickTime_Tick(object sender, EventArgs e)
+        {
+            if(!isFileLinked) { return; }
+            if(!isOn) { return; }
+
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            MainChart.Series["MB"].Points.Clear();
         }
     }
 }
